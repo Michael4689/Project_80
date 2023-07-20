c2cbfe4b77be2eed2395a9c03ca6a4b96a90203a
Michael4689 <128618653+Michael4689@users.noreply.github.com>
2023-07-20 10:26:04 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function show(){
+    var first_name = ("Michael");
+var last_name = ("xue");
+var add = first_name + last_name;
+console.log(add);
+var a=10;
+var b=5;
+var addition = a+b;
+console.log(addition);
+}
